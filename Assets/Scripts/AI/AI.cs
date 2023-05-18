534d89e7beffbd3e65fb7aae3a6606ff08dfdde9
page10 <page10rock@gmail.com>
2023-05-19 01:30:04 +0800

    Update AI.cs

--- Assets/Scripts/AI/AI.cs
+++ Assets/Scripts/AI/AI.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 
     public class AI : MonoBehaviour
     {
-        private CharacterObject cha;
+        private CharacterObject character;
         private List<AIClip> _clips;
         
 
         private void Awake()
         {
-            cha = GetComponent<CharacterObject>();
+            character = GetComponent<CharacterObject>();
         }
 
         public void DoAI()
@@ -21,7 +21,7 @@ using UnityEngine;
                 bool meet = true;
                 foreach (AICondition aiCondition in _clips[i].Conditions)
                 {
-                    if (!aiCondition(cha))
+                    if (!aiCondition(character))
                     {
                         meet = false;
                         break;
@@ -32,11 +32,18 @@ using UnityEngine;
                 {
                     for (int j = 0; j < _clips[i].Actions.Count; j++)
                     {
-                        _clips[i].Actions[j](cha);
+                        _clips[i].Actions[j](character);
                     }
                 }
             }
         }
+        // public AICondition hasAttackableEnemy = (characterObj) =>
+        // {
+        //     List<CharacterObject> attackableEnemies = characterObj.attack.GetAttackableEnemies();
+        //     // 总觉得之前有写过判定攻击范围内是不是有敌人的方法呢……
+        //     return attackableEnemies.Count > 0;
+        // };
+        
     }
 
     public struct AIClip
@@ -44,7 +51,10 @@ using UnityEngine;
         public List<AICondition> Conditions;
         public List<AIAction> Actions;
     }
+// todo 先写死一套ai逻辑绑在敌人身上 大概就是 如果攻击范围内有敌人 就移动并攻击血量最少的敌人 否则待机
 
     public delegate bool AICondition(CharacterObject characterObj);
 
     public delegate void AIAction(CharacterObject characterObj);
+
+
